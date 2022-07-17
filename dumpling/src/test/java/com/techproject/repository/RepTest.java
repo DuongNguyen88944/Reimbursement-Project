44819c5079edc83c505ba5ee72fa85679345083e
Your Name <you@example.com>
2022-07-16 23:21:12 -0500

    update test run

--- dumpling/src/test/java/com/techproject/repository/RepTest.java
+++ dumpling/src/test/java/com/techproject/repository/RepTest.java
@@ -8,32 +8,36 @@ import org.junit.Test;
 
 import com.techproject.entity.Request;
 
-
 public class RepTest {
 
     public static RequestDAOInterface requestDao;
     public static EmployeeDAOInterface employeeDao;
-    
-    
+
     @BeforeClass
-    public static void setup(){
+    public static void setup() {
 
         requestDao = new RequestDAO();
         employeeDao = new EmployeeDAO();
     }
 
     @Test
-    public void getRequestPositiveTest(){
-    List<Request> requestList = requestDao.viewRequest();
-    Assert.assertTrue(requestList.size() > 0);
-}
+    public void getRequestPositiveTest() {
+        List<Request> requestList = requestDao.viewRequest();
+        Assert.assertTrue(requestList.size() > 0);
+    }
 
-    @Test 
-    public void createPositiveRequests(){
-    Request testRequest = new Request("Testing fees", 400, "Pending", "N/A");
-    Request result = requestDao.createRequest(testRequest);
-    Assert.assertNotNull(result.getTicket_number());
-}
+    @Test
+    public void createPositiveRequests() {
+        Request testRequest = new Request("Testing fees", 400, "Pending", "N/A");
+        Request result = requestDao.createRequest(testRequest);
+        Assert.assertNotNull(result.getTicket_number());
+    }
 
+    @Test
+    public void updateBookPositiveTest() {
+        Request updatedRequest = new Request(1, "mile fees", 1000, "updated", "don't want to");
+        Request result = requestDao.updateRequest(updatedRequest);
+        Assert.assertEquals("updated", result.getStatus());
+    }
 
 }
